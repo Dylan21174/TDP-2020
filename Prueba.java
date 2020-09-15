67545453e877f0844bb38424e0c91fdb34253dd4
Dylan <dylan_21174@hotmail.com>
2020-09-15 12:00:41 -0300

    commit inicial

---
+++ Prueba.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoggingPrueba {
+
+	public static void main(String[] args) {
+		Persona cliente1 = new Persona("Martin", 1000);
+		Persona cliente2 = new Persona("Manu", 300);
+	}
+	
+	
+}
